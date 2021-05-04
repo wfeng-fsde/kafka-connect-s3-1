3658f57e41258c4bc07bf6a4df792342e9cca36b
Wei Feng <wfeng@salesforce.com>
2021-05-03 18:30:44 -0700

    logging the key and value for each message for debugging

--- common/src/main/java/com/spredfast/kafka/connect/s3/AlreadyBytesConverter.java
+++ common/src/main/java/com/spredfast/kafka/connect/s3/AlreadyBytesConverter.java
@@ -6,14 +6,20 @@ import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaAndValue;
 import org.apache.kafka.connect.errors.DataException;
 import org.apache.kafka.connect.storage.Converter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * No conversion. Bytes from Kafka are preserved as is and we validate that only bytes go out.
  */
 public class AlreadyBytesConverter implements Converter {
+        private boolean isKey;
+
+	private static final Logger log = LoggerFactory.getLogger(AlreadyBytesConverter.class);
 
 	@Override
 	public void configure(Map<String, ?> configs, boolean isKey) {
+                isKey = isKey;
 	}
 
 	@Override
@@ -21,6 +27,11 @@ public class AlreadyBytesConverter implements Converter {
 		if (schema != null && schema != Schema.BYTES_SCHEMA && schema != Schema.OPTIONAL_BYTES_SCHEMA) {
 			throw new DataException(topic + " error: Not a byte array! " + value);
 		}
+                if (isKey) {
+                        log.warn("key: " + value);
+                } else {
+                        log.warn("value: " + value);
+                }
 		if (value == null) {
 			return null;
 		}
